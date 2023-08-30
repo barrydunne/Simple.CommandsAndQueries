f5c987f7e07e9106750dec064461bf174af14c4b
Barry Dunne <barrydunne@hotmail.com>
2023-08-30 16:49:54 +0100

    Add readme to package

---
+++ PackAndPublish.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory)]$version,
+    [Parameter(Mandatory)]$key
+)
+
+try {
+    Set-Location $PSScriptRoot
+
+    $year = Get-Date -Format "yyyy"
+
+    Write-Host "Building package v$version" -ForegroundColor White -BackgroundColor DarkGreen
+    dotnet pack /p:PackageVersion=$version /p:AssemblyVersion=$version /p:FileVersion=$version /p:Authors="Barry Dunne" /p:Copyright="Copyright © Barry Dunne $year" /p:Description="This is a basic library if interfaces to use for CQRS commands and queries." -c Release Simple.CommandsAndQueries\Simple.CommandsAndQueries.csproj
+
+    Write-Host "Pushing nupkg and snupkg packages v$version to NuGet" -ForegroundColor White -BackgroundColor DarkGreen
+    dotnet nuget push "$PSScriptRoot\Simple.CommandsAndQueries\bin\Release\Simple.CommandsAndQueries.$version.nupkg" -s https://api.nuget.org/v3/index.json -k $key
+   #dotnet nuget push MyAwesomeLib.1.0.0.nupkg -s https://api.nuget.org/v3/index.json -k ~~your API key here~~
+
+    Write-Host "Done" -ForegroundColor White -BackgroundColor DarkGreen
+}
+catch {
+    Write-Host $_.Exception.Message -ForegroundColor Red
+    exit 1
+}
